24d64f1d0d5cd94253e2fb8ce2d8b52981cc7e0e
ik012982i9 <a@>
2024-03-18 12:23:13 +0100

    Class Mision set up finished

--- src/Mision.java
+++ src/Mision.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Mision {
 
     private String idMision;
@@ -12,5 +14,60 @@ public class Mision {
         this.dificultad = dificultad;
     }
 
+    public String getIdMision() {
+        return idMision;
+    }
 
+    public void setIdMision(String idMision) {
+        this.idMision = idMision;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getRecompensa() {
+        return recompensa;
+    }
+
+    public void setRecompensa(String recompensa) {
+        this.recompensa = recompensa;
+    }
+
+    public String getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(String dificultad) {
+        this.dificultad = dificultad;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Mision))
+            return false;
+        Mision mision = (Mision) o;
+        return Objects.equals(getIdMision(), mision.getIdMision()) && Objects.equals(getDescripcion(), mision.getDescripcion()) && Objects.equals(getRecompensa(), mision.getRecompensa()) && Objects.equals(getDificultad(), mision.getDificultad());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getIdMision(), getDescripcion(), getRecompensa(), getDificultad());
+    }
+
+    @Override
+    public String toString() {
+        return "Mision{" +
+                "idMision='" + idMision + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", recompensa='" + recompensa + '\'' +
+                ", dificultad='" + dificultad + '\'' +
+                '}';
+    }
 }
