633ddf5a78076c796c79f2cbef15d05abfdb6e4e
ik012982i9 <a@>
2024-03-18 12:52:48 +0100

    North side page options

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Principal Page");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+
+        // Main container with BorderLayout
+        Container mainContainer = frame.getContentPane();
+        mainContainer.setLayout(new BorderLayout());
+
+        // North area with FlowLayout
+        // JCheckBox for the boxes
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new FlowLayout());
+        northPanel.add(new JButton("News"));
+        northPanel.add(new JButton("Ranking"));
+        northPanel.add(new JButton("Others"));
+        mainContainer.add(northPanel, BorderLayout.NORTH);
 
     }
 }
