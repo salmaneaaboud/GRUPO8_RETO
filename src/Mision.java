c826d565aaa269cbe4318d13e97b3cc3ca3b01e3
ik012982i9 <a@>
2024-03-18 12:22:23 +0100

    Class Mision create

---
+++ src/Mision.java
@@ -0,0 +1,16 @@
+public class Mision {
+
+    private String idMision;
+    private String descripcion;
+    private String recompensa;
+    private String dificultad;
+
+    public Mision(String idMision, String descripcion, String recompensa, String dificultad) {
+        this.idMision = idMision;
+        this.descripcion = descripcion;
+        this.recompensa = recompensa;
+        this.dificultad = dificultad;
+    }
+
+
+}
